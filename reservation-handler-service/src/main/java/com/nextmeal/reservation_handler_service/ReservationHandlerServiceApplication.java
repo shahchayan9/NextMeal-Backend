92fde55f296d92c177915b6ca212baa177f04254
Yash Kumar <yash.arunkumar@gmail.com>
2024-11-20 15:10:52 -0800

    Added init spring code for reservation services

---
+++ reservation-handler-service/src/main/java/com/nextmeal/reservation_handler_service/ReservationHandlerServiceApplication.java
@@ -0,0 +1,11 @@
+package com.nextmeal.reservation_handler_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationHandlerServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationHandlerServiceApplication.class, args);
+	}
+}
---
+++ reservation-queue-service/src/test/java/com/nextmeal/reservation_queue_service/ReservationQueueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextmeal.reservation_queue_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationQueueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
