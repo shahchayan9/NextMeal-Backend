6f1cd059d71bee3d9127603704ed5d767f236014
shah-chayan9 <chayan.shah@cogoport.com>
2024-11-23 00:21:43 -0800

    fix

--- Restaurant/config.py
+++ Restaurant/config.py
@@ -2,11 +2,11 @@ import os
 
 class Config:
     # PostgreSQL configuration
-    SQLALCHEMY_DATABASE_URI = 'postgresql://chayanshah:admin@localhost/nextmeal'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DB_URL', 'postgresql://chayanshah:admin@localhost/nextmeal')  # Default value for local dev
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # Redis configuration
-    REDIS_URL = "redis://localhost:6379/0"
+    # Redis configuration (default for local dev)
+    REDIS_URL = os.getenv('REDIS_URL', "redis://localhost:6379/0")
 
     # Secret key for sessions or cookies (you can change this)
     SECRET_KEY = os.urandom(24)
