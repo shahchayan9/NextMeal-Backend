323960ee9bfd2722b1ce78c4728c78d44e7a6d26
shah-chayan9 <chayan.shah@cogoport.com>
2024-12-05 04:26:54 -0800

    fix

--- restaurant-service/app.py
+++ restaurant-service/app.py
@@ -35,4 +35,4 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True, host='0.0.0.0', port=5001)  # 0.0.0.0 allows external access
+    app.run(debug=True, host='0.0.0.0', port=5004)  # 0.0.0.0 allows external access
